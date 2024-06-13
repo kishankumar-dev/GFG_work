dcb8437470fd39a4848202196aa4dbdaf327b906
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2024-06-13 06:46:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Padovan Sequence/padovan-sequence.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def padovanSequence(self, n):
+        # code here 
+        MOD = 10**9 + 7
+        a = b = c = 1
+        
+        for i in range(3, n + 1):
+            a, b, c = b, c, (a+ b) % MOD
+            
+        return c % MOD
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        ob = Solution()
+        print(ob.padovanSequence(n))
+
+# } Driver Code Ends
