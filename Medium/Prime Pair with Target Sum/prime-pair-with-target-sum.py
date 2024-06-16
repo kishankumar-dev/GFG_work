e296f99bcbb1a53ffaa3a2433ef4ff35d9f69e46
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2024-06-16 22:10:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prime Pair with Target Sum/prime-pair-with-target-sum.py
@@ -0,0 +1,55 @@
+
+from typing import List
+
+
+class Solution:
+    def getPrimes(self, n : int) -> List[int]:
+        # code here
+        for a in range(2, n // 2 + 1):
+            b = n - a
+            if self.is_prime(a) and self.is_prime(b):
+                return [a, b]
+        return [-1, -1]
+    def is_prime(self, num):
+        if num == 2 or num == 3:
+            return True
+        if num % 2 == 0 or num % 3 == 0:
+            return False
+        i = 5
+        while i * i <= num:
+            if num % i == 0 or num % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        obj = Solution()
+        res = obj.getPrimes(n)
+
+        IntArray().Print(res)
+
+# } Driver Code Ends
