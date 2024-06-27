14de983dc7d91a8273d6ef0a30b8bfc3efafdb3c
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2024-06-27 10:26:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Toeplitz matrix/toeplitz-matrix.py
@@ -0,0 +1,33 @@
+# You are required to complete this method
+# Return True/False or 1/0
+def isToepliz(mat):
+    #code here
+    rows, cols = len(mat), len(mat[0])
+    for i in range(rows - 1):
+        for j in range(cols - 1):
+            if mat[i][j]!= mat[i + 1][j + 1]:
+                return 0
+    return 1
+
+#{ 
+ # Driver Code Starts
+# Your code goes here
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n, m = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        matrix = [[0 for i in range(m)] for j in range(n)]
+        k = 0
+        for i in range(n):
+            for j in range(m):
+                matrix[i][j] = arr[k]
+                k += 1
+        b = isToepliz(matrix)
+
+        if b == True:
+            print("true")
+        else:
+            print("false")
+
+# } Driver Code Ends
