e9aa59e60a0918a0fe9cf65fa93ca874b3f234c9
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2024-09-26 07:10:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roof Top/roof-top.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find maximum number of consecutive steps 
+    #to gain an increase in altitude with each step.
+    def maxStep(self, arr):
+        #Your code here
+        ct = mx = 0
+        for i in range(1,len(arr)):
+            if arr[i] > arr[i-1]: ct += 1
+            else: ct = 0
+            mx = max(mx,ct)
+        return mx  
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.maxStep(arr))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
