4fd370a0c06d0a12c0a593c7daaa2ba161ce0ead
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2024-12-11 07:41:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.py
@@ -0,0 +1,29 @@
+class Solution:
+    def mergeArrays(self, a, b):
+        # code here
+        merged = sorted(a + b)
+        a[:] = merged[:len(a)]
+        b[:] = merged[len(a):]
+
+#{ 
+ # Driver Code Starts
+# Input handling and main function
+if __name__ == "__main__":
+    # Number of test cases
+    t = int(input().strip())
+
+    for _ in range(t):
+        # Input first array
+        a = list(map(int, input().strip().split()))
+        # Input second array
+        b = list(map(int, input().strip().split()))
+
+        # Create solution object and merge the arrays
+        solution = Solution()
+        solution.mergeArrays(a, b)
+
+        # Output both arrays in the same line space-separated
+        print(" ".join(map(str, a)))
+        print(" ".join(map(str, b)))
+
+# } Driver Code Ends
