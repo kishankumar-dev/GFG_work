b40e10af0ed78ef63790a09d5e27744e9d047a7f
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2025-01-17 10:14:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.py
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.py
@@ -1,33 +1,19 @@
 #User function Template for python3
 
 class Solution:
-    def productExceptSelf(self, nums):
+    def productExceptSelf(self, arr):
         #code here
-        c0 = nums.count(0)
-        if (c0 == 1):
-            p = 1
-            for i in nums:
-                if i != 0:
-                    p = p*i
-            for i in range(len(nums)):
-                if nums[i] != 0:
-                    nums[i] = 0
-                else:
-                    nums[i] = p
-            return nums
-        elif c0==len(nums):
-            return nums
-        elif c0>1:
-            for i in range(len(nums)):
-                nums[i] = 0
-            return nums
-        else:
-            p = 1
-            for i in nums:
-                p = p*i
-            for i in range(len(nums)):
-                nums[i] = p//nums[i]
-            return nums
+        n = len(arr)
+        res, lp, rp = [1]*n, [1]*n, [1]*n
+        lp[0], rp[n-1] = arr[0], arr[n-1]
+        tmp = 1
+        for i in range(1,n):
+            lp[i] =  lp[i-1]*arr[i]
+            rp[n-1-i] = rp[n-i]*arr[n -1 - i]
+        res[0], res[n-1] = rp[1], lp[n-2] 
+        for i in range(1,n-1):
+            res[i] = (lp[i-1]*rp[i+1])
+        return res
 
 #{ 
  # Driver Code Starts
@@ -37,10 +23,11 @@ if __name__ == '__main__':
     t = int(input())
 
     for _ in range(t):
-        n = int(input())
+
         arr = [int(x) for x in input().split()]
 
         ans = Solution().productExceptSelf(arr)
         print(*ans)
+        print("~")
 
 # } Driver Code Ends
