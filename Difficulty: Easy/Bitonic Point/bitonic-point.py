759dfbd5467507719f8512f68ef6e65ec26dc770
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2025-05-02 10:00:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bitonic Point/bitonic-point.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+class Solution:
+
+	def findMaximum(self, arr):
+		# code here
+        n=len(arr)
+        for i in range(n+1):
+            if arr[i]<arr[i+1]:
+                pass
+            else:
+                return arr[i]
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.findMaximum(arr)
+        print(ans)
+        tc -= 1
+        print("~")
+
+# } Driver Code Ends
