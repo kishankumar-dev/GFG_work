0b632d02c524ed31d0745e19f1170d3f0eadb23d
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2025-05-13 09:08:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+    def nCr(self, n, r):
+        # code here
+        if r > n:
+            return 0
+        
+        if r > n - r:
+            r = n - r
+
+        result = 1        
+        for i in range(r):
+            result *=(n-i)
+            result /=(i+1)
+        return int(result)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        r = int(input())
+        ob = Solution()
+        print(ob.nCr(n, r))
+        print("~")
+# } Driver Code Ends
