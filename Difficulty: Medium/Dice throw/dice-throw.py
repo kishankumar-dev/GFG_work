c183adfcc5ee971a2733e74db03425b686f34c9f
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2025-05-23 07:56:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dice throw/dice-throw.py
@@ -0,0 +1,11 @@
+class Solution:
+    def noOfWays(self, m,n,x):
+        # code here
+        reachable = defaultdict(int, {0: 1})
+        for _ in range(n):
+            new_reachable = defaultdict(int)
+            for k, v in reachable.items():
+                for km in range(k + 1, min(x, k + m) + 1):
+                    new_reachable[km] += v
+            reachable = new_reachable
+        return reachable[x]
