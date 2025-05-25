7ddfe3709d4aea699b7396b00ec615b0fce6aedb
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2025-05-25 10:49:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.py
@@ -0,0 +1,11 @@
+class Solution:
+	def pythagoreanTriplet(self, arr):
+    	# code here
+        squared = [x * x for x in arr]
+        squared_set = set(squared)
+        n = len(squared)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if (squared[i] + squared[j]) in squared_set:
+                    return True
+        return False
