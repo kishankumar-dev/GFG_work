e659b6d36011f94dec97e33a0005c87fe03a1671
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2025-09-01 08:49:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Sum of Mode/sum-of-mode.py
@@ -0,0 +1,47 @@
+class Solution:
+    def sumOfModes(self, arr, k):
+        # code here
+        freq = defaultdict(int)         
+        count_map = defaultdict(set)
+        max_freq = 0                    
+        result = 0
+
+        for i in range(len(arr)):
+            # Add new element to window
+            num = arr[i]
+            prev_freq = freq[num]
+            freq[num] += 1
+            new_freq = freq[num]
+
+            if prev_freq > 0:
+                count_map[prev_freq].discard(num)
+                if not count_map[prev_freq]:
+                    del count_map[prev_freq]
+
+            count_map[new_freq].add(num)
+            max_freq = max(max_freq, new_freq)
+
+            # Remove element outside window
+            if i >= k:
+                out_num = arr[i - k]
+                out_freq = freq[out_num]
+                freq[out_num] -= 1
+
+                count_map[out_freq].discard(out_num)
+                if not count_map[out_freq]:
+                    del count_map[out_freq]
+
+                if freq[out_num] > 0:
+                    count_map[freq[out_num]].add(out_num)
+                else:
+                    del freq[out_num]
+
+                # Recalculate max_freq if needed
+                if max_freq not in count_map:
+                    max_freq = max(count_map.keys(), default=0)
+
+            # Add mode to result
+            if i >= k - 1:
+                result += min(count_map[max_freq])
+
+        return result
