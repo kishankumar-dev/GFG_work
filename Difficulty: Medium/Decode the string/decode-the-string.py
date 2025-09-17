be2301b1fa37fc1160116305c77c27b6e8583069
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2025-09-17 09:50:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.py
+++ Difficulty: Medium/Decode the string/decode-the-string.py
@@ -1,47 +1,32 @@
-
+from collections import deque
 class Solution:
     def decodedString(self, s):
-        # code here
-        n = len(s)
-        stopper = "-1"
-        def parse(s, i):
-            r = ""
-            while i < n:
-                p, i = parse_item(s, i)
-                if p == stopper:
-                    break
-                r += p
-            return r, i
-        def parse_item(s, i):
-            if i >= n:
-                return "", n
-            if s[i] == '[':
-                return parse(s, i+1)
-            if s[i] == ']':
-                return stopper, i+1
-            if not s[i].isdigit():
-                return s[i], i+1
-            e = 0
-            while s[i].isdigit():
-                e = e*10 + ord(s[i]) - ord('0')
-                i += 1
-            r, i = parse_item(s, i)
-            return r*e, i
-        
-        r, _ = parse(s, 0)
-        return r
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        s = input()
-
-        ob = Solution()
-        print(ob.decodedString(s))
-        print("~")
-
-# } Driver Code Ends
+        # code here  
+        stack=[]
+        num=[]
+        op=""
+        j=0
+        while j<len(s):
+            ch=s[j]
+            if ch.isdigit():
+                if j>0 and s[j-1].isdigit():
+                    prev+=ch
+                    num.pop()
+                    num.append(prev)
+                else:
+                    num.append(ch)
+                    prev=ch
+                    
+            elif ch=='[' or 'a'<=ch<="z":
+                stack.append(ch)
+                
+            else:
+                temp=deque([])
+                di=int(num.pop()) if num else 1
+                while stack and stack[-1]!='[':
+                    temp.appendleft(stack.pop())
+                if stack:
+                    stack.pop()
+                stack.append(("".join(temp))*di)
+            j+=1
+        return "".join(stack)
