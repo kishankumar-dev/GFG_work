3499618bff284f7d2be62478ac889e778e8a1119
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2025-11-25 19:49:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Game of XOR/game-of-xor.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subarrayXor(self, arr):
+        # code here 
+        len1 = res = 0
+        for i in arr:
+            if not len1%2:
+                res = res^arr[len1]
+            len1+=1
+        return res if len1%2 else 0
