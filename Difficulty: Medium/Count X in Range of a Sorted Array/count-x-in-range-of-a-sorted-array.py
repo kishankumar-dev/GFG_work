141ed425ddb1b7e1b7798c2f971a1995e3b18f51
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2025-12-21 09:37:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count X in Range of a Sorted Array/count-x-in-range-of-a-sorted-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countXInRange(self, arr, queries):
+        # code here
+        res=[]
+        for l,r,x in queries:
+            lb = bisect.bisect_left(arr, x, lo=l, hi=r+1)
+            ub = bisect.bisect_right(arr, x, lo=l, hi=r+1)
+            res.append(ub-lb)
+        return res
