4122fca15ce0ac652b8a6a0cf9e13fa4c03a5da7
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-01-05 10:27:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubarraySum(self, arr, k):
+        # code here 
+        cont_sum = res = sum(arr[:k])
+        for i in range(k,len(arr)):
+            cont_sum = cont_sum - arr[i-k] + arr[i]
+            res = max(cont_sum,res)
+        return res
