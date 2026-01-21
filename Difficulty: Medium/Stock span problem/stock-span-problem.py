6c08cb591d380577cbb6eefe91fa4ae33981d4b0
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-01-21 10:24:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.py
+++ Difficulty: Medium/Stock span problem/stock-span-problem.py
@@ -1,35 +1,11 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
 class Solution:
     def calculateSpan(self, arr):
-        #write code here    
-        n = len(arr)
-        ans = [0] * n
-        stack = []
-        for i in range(n):
-            while stack and arr[stack[-1]] <= arr[i]:
-                stack.pop()
-            if not stack:
-                ans[i] = i + 1
-            else:
-                ans[i] = i - stack[-1]
-            stack.append(i)
+        # code here
+        s = [(10**5 + 1, -1)]
+        ans = []
+        for i, a in enumerate(arr):
+            while s[-1][0] <= a:
+                s.pop()
+            ans.append(i - s[-1][1])
+            s.append((a, i))
         return ans
-
-#{ 
- # Driver Code Starts.
-#Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        arr = list(map(int, input().split()))
-        ob = Solution()
-        ans = ob.calculateSpan(arr)
-        print(*ans)
-        print("~")
-        t -= 1
-# } Driver Code Ends
