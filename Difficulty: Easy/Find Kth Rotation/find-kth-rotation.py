3861d360d88d95075c9cc67e81005d979ebe22d3
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-02-09 19:50:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKRotation(self, arr):
+        # code here
+        sortedArr=sorted(arr)
+        maxEle=max(arr)
+        if sortedArr!=arr:
+            ans=arr.index(maxEle)
+            return  ans+1
+        return 0
