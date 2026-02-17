eeab730c58100d5719ca9615dfa2626641b84339
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-02-17 08:54:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Maximum number of overlapping Intervals/maximum-number-of-overlapping-intervals.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+class Solution:
+    def overlapInt(self, arr):
+        # code here
+        cnt=defaultdict(int)
+        for sta,sto in arr:
+            cnt[sta]+=1
+            cnt[sto+1]-=1
+        mx=cur=0
+        for ix in sorted(cnt):
+            cur+=cnt[ix]
+            mx=max(mx,cur)
+        return mx      
