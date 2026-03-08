26cc1a965b5deae149518acbe6dd70462c1df3b7
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-03-08 10:50:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.py
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.py
@@ -1,11 +1,21 @@
 class Solution:
 	def pythagoreanTriplet(self, arr):
     	# code here
-        squared = [x * x for x in arr]
-        squared_set = set(squared)
-        n = len(squared)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if (squared[i] + squared[j]) in squared_set:
+    	MAX = 1000
+        present = [False]*(MAX+1)
+        for num in arr:
+            present[num] = True
+            
+        for a in range(1, MAX+1):
+            if not present[a]:
+                continue
+            
+            for b in range(a+1, MAX+1):
+                if not present[b]:
+                    continue
+                
+                squared = (a*a+b*b)
+                c = int(squared**0.5)
+                if c <= MAX and c*c == squared and present[c]:
                     return True
         return False
