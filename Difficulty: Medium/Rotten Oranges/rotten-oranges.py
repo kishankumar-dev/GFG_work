a2d026bc81a89ce0bc3eba71a09849e2e3fb628e
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-03-22 17:41:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotten Oranges/rotten-oranges.py
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.py
@@ -1,37 +1,30 @@
 class Solution:
-	def orangesRotting(self, mat):
-		#Code here
-    	q = [(i, j) for i, r in enumerate(mat) for j, e in enumerate(r) if e == 2]
+	def orangesRot(self, mat):
+		# code here
+        hth=len(mat)
+        wth=len(mat[0])
+        cnt=sum(rw.count(1) for rw in mat)
+        if cnt==0:
+            return 0
+        t=0
+        q=[(x,y,) for x in range(wth) for y in range(hth) if mat[y][x]==2]
+        while q:
+            nq=[]
+            for x,y in q:
+                mat[y][x]=0
+                for dx,dy in [(0,1,),(0,-1,),(1,0,),(-1,0,),]:
+                    nx=x+dx
+                    ny=y+dy
+                    if not(0<=nx<wth and 0<=ny<hth):
+                        continue
+                    if mat[ny][nx]!=1:
+                        continue
+                    mat[ny][nx]=2
+                    nq.append((nx,ny,))
+                    cnt-=1
+            q=nq
+            t+=1
+            if cnt==0:
+                return t
+        return -1
 
-		m, n, t = len(mat), len(mat[0]), 0
-		while q:
-		    nq = []
-		    for r0, c0 in q:
-		        for r, c in ((r0+1, c0), (r0-1, c0), (r0, c0+1), (r0, c0-1)):
-		            if 0 <= r < m and 0 <= c < n and mat[r][c] == 1:
-		                nq.append((r, c))
-		                mat[r][c] = 2
-		    if nq:
-		        q = nq
-		        t += 1
-            q = nq
-	
-	    return -1 if any([e == 1 for r in mat for e in r]) else t
-#{ 
- # Driver Code Starts
-from queue import Queue
-
-T = int(input())
-for i in range(T):
-    n = int(input())
-    m = int(input())
-    mat = []
-    for _ in range(n):
-        a = list(map(int, input().split()))
-        mat.append(a)
-    obj = Solution()
-    ans = obj.orangesRotting(mat)
-    print(ans)
-    print("~")
-
-# } Driver Code Ends
