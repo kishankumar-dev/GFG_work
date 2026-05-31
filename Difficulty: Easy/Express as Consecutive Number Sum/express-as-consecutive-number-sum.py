2ba7458c13eaab860ec7d0787b630ea2e1bd7bd6
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-05-31 12:52:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Express as Consecutive Number Sum/express-as-consecutive-number-sum.py
@@ -0,0 +1,7 @@
+import math
+class Solution:
+    def isSumOfConsecutive(self, n: int) -> bool:
+        if math.log2(n) == int(math.log2(n)):
+            return False
+            
+        return True
