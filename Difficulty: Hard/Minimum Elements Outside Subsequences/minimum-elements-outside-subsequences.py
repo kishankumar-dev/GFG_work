f34323b4c0a10673b6a0754ae658fabe50a964a8
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-09-07 10:48:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum Elements Outside Subsequences/minimum-elements-outside-subsequences.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCount(self, arr):
+            from functools import cache
+            n = len(arr)
+
+            @cache
+            def dfs(i: int = 0, inc: int = -1, dec: int = -1) -> int:
+                if i == n:
+                    return 0
+                used = dfs(i + 1, inc, dec)
+                a = arr[i]
+                if inc == -1 or a > inc:
+                    used = max(used, 1 + dfs(i + 1, a, dec))
+                if dec == -1 or a < dec:
+                    used = max(used, 1 + dfs(i + 1, inc, a))
+                return used
+
+            return n - dfs()
