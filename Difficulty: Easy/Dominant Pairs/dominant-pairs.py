ca93e00513ae03ff50b06f1f2e00323bcc5ea955
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-09-16 09:28:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Dominant Pairs/dominant-pairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dominantPairs(self, arr: list[int]) -> int:
+        half = len(arr) // 2
+        lower = sorted(arr[:half])
+        upper = sorted(arr[half:])
+        i = count = 0
+        for val in lower:
+            while i < half and val >= 5 * upper[i]:
+                i += 1
+            count += i
+        return count
